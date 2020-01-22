3d356fdf20bb48527a7f5a88bc464be514e45ff2
Kyle Caprio <capriok7@yahoo.com>
2020-01-21 22:04:32 -0700

    touches

--- src/components/primary.js
+++ src/components/primary.js
@@ -12,7 +12,7 @@ export default function Primary() {
       </div>
         <div className="two">
           <div className='two-content'>
-            <h1>Sample content placeholder</h1>
+            <h1>Sample content placeholder.</h1>
             <p>This is a brief greeting to the customer, reeling them in, featuring a product/service. This is a brief greeting to the customer, reeling them in, featuring a product/service</p>
             <p>The user will then explore the site farther and know what theyre exploring</p>
           </div> 
