341b8b5e03044efd9dc4e1a231d822a2aeb534d5
Kyle Caprio <capriok7@yahoo.com>
2020-01-25 14:21:19 -0700

    touches

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import banner from "../img/banner.jpg";
 
-export default function Banenr() {
+export default function Banner() {
   return (
     <>
       <div className="banner-main">
