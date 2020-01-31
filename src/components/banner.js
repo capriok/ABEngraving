13711dbdd2ee79599b2a5fb5e434339a3e3a12b2
Kyle Caprio <capriok7@yahoo.com>
2020-01-30 19:54:14 -0700

    touches

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import banner from "../img/banner.jpg";
-import banner2 from "../img/banner2.jpg";
+import banner from "../img/banner.jpg";
 
 export default function Banner() {
   return (
     <>
       <div className="banner-main">
-        <img className="banner-img" src={banner2} alt="" />
+        <img className="banner-img" src={banner} alt="" />
       </div>
     </>
   );
